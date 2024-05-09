190b7e35ce1a4e79f5113b010a6187f9e23c54c7
TelevisionNinja <TelevisionNinja@gmail.com>
2024-05-09 16:35:17 -0500

    use llama.cpp repo

--- launch.sh
+++ launch.sh
@@ -1,1 +1,1 @@
-./src/talk.exe --session ./sessionfile -mw ./models/ggml-small.en-q5_1.bin -ml ./models/llama-2-7b-chat.Q4_K_M.gguf -p "TelevisionNinja" -t 12 -bn "Fluttershy" -w "Fluttershy"
+./src/llama.cpp/talk.exe --session ./sessionfile -mw ./models/ggml-small.en-q5_1.bin -ml ./models/Meta-Llama-3-8B-Instruct-IQ4_XS.gguf -p "TelevisionNinja" -t 12 -bn "Fluttershy"
