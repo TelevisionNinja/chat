05998756eee071546d1131f4d7673083510d8721
TelevisionNinja <TelevisionNinja@gmail.com>
2024-05-21 16:24:56 -0500

    updated launch scripts

--- launch.ps1
+++ launch.ps1
@@ -1,2 +1,2 @@
 Start-Process python -ArgumentList ".\src\TTSServer.py"
-./src/llama.cpp/build/bin/Release/talk.exe --session ./sessionfile -mw ./models/ggml-small.en-q5_1.bin -ml ./models/Meta-Llama-3-8B-Instruct-IQ4_XS.gguf -p "TelevisionNinja" -t 12 -bn "Fluttershy" -s "powershell .\\src\\llama.cpp\\common\\talk\\speak.ps1"
+./src/llama.cpp/build/bin/Release/talk.exe --session ./sessionfile -mw ./models/ggml-small.en-q5_1.bin -ml ./models/Meta-Llama-3-8B-Instruct-IQ4_XS.gguf -p "TelevisionNinja" -t 12 -bn "Fluttershy" -s "python .\\src\\TTSClient.py"
